22dd96bcc1d8ffaabd01d2477b24a8b64f771965
unknown <Nightmachine@gmail.com>
2019-11-12 11:43:35 -0700

    pulling in player stats

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import './App.css';
 export default class Odub extends Component {
   state = {
     username: '',
-    portrait: ''
+    portrait: '',
+    level: '',
+    quickPlayTime: '',
+    levelFrame: '',
+    star: ''
   }
 
   componentDidMount() {
-    axios.get(`http://overwatchy.com/profile/pc/us/Inphinion-1371`).then(res => {
+    axios.get(`http://overwatchy.com/profile/pc/us/anrew-11861`).then(res => {
       console.log(res)
       this.setState({
         username: res.data.username,
-        portrait: res.data.portrait
+        portrait: res.data.portrait,
+        level: res.data.level,
+        quickPlayTime: res.data.playtime.quickplay,
+        levelFrame: res.data.levelFrame,
+        star: res.data.star
       })
     })
   }
@@ -22,7 +30,11 @@ export default class Odub extends Component {
     return (
       <div>
         <img src={this.state.portrait}></img>
-        {this.state.username}
+        <img src={this.state.levelFrame}></img>
+        <img src={this.state.star}></img>
+        <div><p>Player Name: {this.state.username}</p></div>
+        <div><p>Level: {this.state.level}</p></div>
+        <div><p>Total Quickplay Time: {this.state.quickPlayTime}</p></div>
       </div>
     )
   }
