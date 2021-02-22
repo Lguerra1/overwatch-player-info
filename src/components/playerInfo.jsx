d3104107d19b38327f2b9f8ea0d083dd30466a78
larry <Nightmachine@gmail.com>
2021-02-22 08:22:07 -0700

    switched to old api, faster

--- src/components/playerInfo.jsx
+++ src/components/playerInfo.jsx
@@ -44,28 +44,17 @@ const PlayerInfo = () => {
     // Inphinion-1371
     // BoomyZoomies-1815
 
-    // competitive: { rank: null, rank_img: null }
-    // endorsement: { sportsmanship: { … }, shotcaller: { … }, teammate: { … }, level: null, frame: "https://static.playoverwatch.com/svg/icons/endorsement-frames-3c9292c49d.svg#_3", … }
-    // games: { quickplay: { … }, competitive: { … } }
-    // level: 748
-    // levelFrame: " https://d15f34w2p8l1cc.cloudfront.net/overwatch/ba68d2c0f1b55e1991161cb1f88f369b97311452564b200ea1da226eb493e2e8.png "
-    // playtime: { quickplay: "611:19:18", competitive: "21:48" }
-    // portrait: "https://d15f34w2p8l1cc.cloudfront.net/overwatch/5d60fae93b5b8bc2161bf03cb1c55f148d76ccaee6e5ac8d0f639bcf0587e95c.png"
-    // private: false
-    // star: " https://d15f34w2p8l1cc.cloudfront.net/overwatch/426c754c76cd12e6aacd30293a67363571341eea37880df549d3e02015a588fe.png "
-    // username: "Inphinion"
-
     return (
         <PlayerInfoSection name="playerinfosection">
             <UserInfoWrapper>
                 <SectionTitle>
-                    {searchResults?.username ? <h3>{searchResults.username} - Level: {searchResults.level}</h3> : null}
+                    {searchResults?.name ? `${searchResults.name} - Level: ${searchResults.level}` : null}
                 </SectionTitle>
                 <Marginer direction="vertical" margin="3em" />
                 <UserStats>
-                    {searchResults?.portrait ? <img src={searchResults.portrait} alt='player portrait' /> : null}
-                    {searchResults?.playtime?.quickplay ? <p>Quickplay Time: {searchResults.playtime.quickplay}</p> : null}
-                    {searchResults?.games?.quickplay?.won ? <p>Quickplay Games Won: {searchResults.games.quickplay.won}</p> : null}
+                    {searchResults?.icon ? <img src={searchResults.icon} alt='player portrait' /> : null}
+                    {searchResults?.gamesWon ? <p>Games Won: {searchResults.gamesWon}</p> : null}
+                    {searchResults?.endorsement ? <p>Endorsement: {searchResults.endorsement}</p> : null}
                 </UserStats>
             </UserInfoWrapper>
         </PlayerInfoSection>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -39,11 +39,10 @@ const Search = () => {
 
         e.preventDefault();
         e.stopPropagation();
-        // formatBattleTag(battleTag);
-         
+        // formatBattleTag(battleTag);        
 
         try {
-            const response = await fetch(`https://cors-anywhere.herokuapp.com/https://best-overwatch-api.herokuapp.com/player/pc/us/${battleTag}`, {
+            const response = await fetch(`https://cors-anywhere.herokuapp.com/https://ow-api.com/v1/stats/pc/us/${battleTag}/complete`, {
                 mode: 'cors',
                 headers: {
                     'Access-Control-Allow-Origin': '*'
@@ -66,21 +65,6 @@ const Search = () => {
     //     return value;
     // }
 
-    //   competitiveStats: {awards: {cards: 1, medals: 6, medalsBronze: 3, medalsSilver: 0, medalsGold: 3},…}
-    // endorsement: 3
-    // endorsementIcon: "https://static.playoverwatch.com/svg/icons/endorsement-frames-3c9292c49d.svg#_3"
-    // gamesWon: 2675
-    // icon: "https://d15f34w2p8l1cc.cloudfront.net/overwatch/5d60fae93b5b8bc2161bf03cb1c55f148d76ccaee6e5ac8d0f639bcf0587e95c.png"
-    // level: 47
-    // levelIcon: "https://d15f34w2p8l1cc.cloudfront.net/overwatch/ba68d2c0f1b55e1991161cb1f88f369b97311452564b200ea1da226eb493e2e8.png"
-    // name: "Inphinion#1371"
-    // prestige: 7
-    // prestigeIcon: "https://d15f34w2p8l1cc.cloudfront.net/overwatch/426c754c76cd12e6aacd30293a67363571341eea37880df549d3e02015a588fe.png"
-    // private: false
-    // quickPlayStats: {awards: {cards: 2041, medals: 12320, medalsBronze: 4351, medalsSilver: 4491, medalsGold: 3478},…}
-    // rating: 0
-    // ratingIcon: ""
-    // ratings: null
     const scrollToNextSection = () => {
         scroller.scrollTo("playerinfosection", { smooth: true, duration: 1500 })
     }
